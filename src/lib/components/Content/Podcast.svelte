c18298fd4ce2e5de3e20cb2f993fb35942651e7c
Tsen Chung <tsen.fchung@gmail.com>
2022-05-07 02:43:14 +0000

    Podcast, dark theme, and better light theme

--- src/lib/components/Content/Podcast.svelte
+++ src/lib/components/Content/Podcast.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import './Podcast.scss';
+  import Amplitude from 'amplitudejs';
+  import PodcastCoverArt from './chompipera-cover-light.webp';
+
   import type { Entry } from '$lib/types/Entry';
-  import Title from '$lib/components/Title.svelte';
-  import Podcast from './Podcast.svelte';
+  import { onMount, onDestroy } from 'svelte';
   export let entry: Entry = {
     title: '',
     author: '',
@@ -13,7 +16,58 @@
     category: 'podcast',
     podcastUrl: 'http://someurl'
   };
+
+  onMount(() => {
+    Amplitude.init({
+      bindings: {
+        37: 'prev',
+        39: 'next',
+        32: 'play_pause'
+      },
+      songs: [
+        {
+          name: 'La Chompipera: El complot',
+          artist: 'La Cuarta Pared',
+          album: 'We Are to Answer',
+          url: "https://521dimensions.com/song/Ancient Astronauts - Risin' High (feat Raashan Ahmad).mp3",
+          cover_art_url: PodcastCoverArt
+        }
+      ]
+    });
+    document.getElementById('song-played-progress').addEventListener('click', function (e) {
+      var offset = this.getBoundingClientRect();
+      var x = e.pageX - offset.left;
+
+      Amplitude.setSongPlayedPercentage((parseFloat(x) / parseFloat(this.offsetWidth)) * 100);
+    });
+  });
+
+  onDestroy(() => {
+    Amplitude.stop();
+  });
 </script>
 
-<Title {entry} />
 <section class="content"><slot /></section>
+<div id="audio-player">
+  <img data-amplitude-song-info="cover_art_url" alt="" />
+  <div class="bottom-container">
+    <progress class="amplitude-song-played-progress" id="song-played-progress" />
+
+    <div class="time-container">
+      <span class="current-time">
+        <span class="amplitude-current-minutes" />:<span class="amplitude-current-seconds" />
+      </span>
+      <span class="duration">
+        <span class="amplitude-duration-minutes" />:<span class="amplitude-duration-seconds" />
+      </span>
+    </div>
+
+    <div class="control-container">
+      <div class="amplitude-play-pause" id="play-pause" />
+      <div class="meta-container">
+        <span data-amplitude-song-info="name" class="song-name" />
+        <span data-amplitude-song-info="artist" />
+      </div>
+    </div>
+  </div>
+</div>
